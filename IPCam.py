a58c24ab25a0dd108fd85595d6b14a77e2867cd9
ASUJAYKK <78997596+asujaykk@users.noreply.github.com>
2021-12-04 20:28:12 +0530

    Add files via upload
    1. urllib cannot be used with python3, so urllib3 module used instead  
    2. The Class method  "getLatestFrame(self)", removed since it need some update to work with urllib3, (This method was redundant  because getFrames() method can provide latest frames)
    3. New class method "httpRequest(url)" added. This method will help to update the Camera module easily , if urrllib module have any api changes in future release.

--- IPCam.py
+++ IPCam.py
@@ -7,13 +7,13 @@ Created on Sun Jan 12 19:52:23 2020
 
 
 import cv2
-import urllib
+import urllib3
 import numpy as np
 #import json
+http = urllib3.PoolManager()
 
 class IPCam():
     active_cameras = 0
-    http = urllib
     #focus modes
     auto = 'auto'
     manual = 'off'
@@ -34,18 +34,18 @@ class IPCam():
     def getFrames(self):
         return(self.cap.read())
         
-    def getLatestFrame(self):
-        pic = '/shot.jpg'
-        url_response= (IPCam.http.request.urlopen(self.url +pic))
-        img_array = np.array(bytearray(url_response.read()), dtype=np.uint8)
-        img = cv2.imdecode(img_array, -1)
-        return img
+    # def getLatestFrame(self):
+    #     pic = '/shot.jpg'
+    #     url_response= (self.httpRequest(self.url +pic))
+    #     img_array = np.array(bytearray(url_response.read()), dtype=np.uint8)
+    #     img = cv2.imdecode(img_array, -1)
+    #     return img
     def setZoom(self, zoom_value,zoom_min = 0,zoom_max=100):
         #zoom_max= 100
         zoom_inc = 0.01
         zoom_cam = '/ptz?zoom='
         if zoom_value >= zoom_min and zoom_value <= zoom_max:
-            IPCam.http.request.urlopen( self.url + zoom_cam + str(zoom_value))
+            self.httpRequest( self.url + zoom_cam + str(zoom_value))
         elif zoom_value < zoom_min:
             return zoom_value - zoom_min
         else:
@@ -57,7 +57,7 @@ class IPCam():
         qual_inc = 1
         qual_cam = '/settings/quality?set='
         if quality_val >= qual_min and quality_val <= qual_max:
-            IPCam.http.request.urlopen( self.url + qual_cam + str(quality_val))
+            self.httpRequest( self.url + qual_cam + str(quality_val))
         elif quality_val < qual_min:
             return quality_val - qual_min
         else:
@@ -70,10 +70,10 @@ class IPCam():
         expo_inc = 1
         expo_cam = '/settings/exposure_ns?set='       
         expo_mode= '/settings/manual_sensor?set=on'
-        IPCam.http.request.urlopen( self.url + expo_mode ) #setting expo mode to manula first
+        self.httpRequest( self.url + expo_mode ) #setting expo mode to manual first
         
         if exp_val >= expo_min and exp_val <= expo_max:
-            IPCam.http.request.urlopen( self.url + expo_cam + str(exp_val))
+            self.httpRequest( self.url + expo_cam + str(exp_val))
         elif exp_val < expo_min:
             return exp_val - expo_min
         else:
@@ -82,24 +82,24 @@ class IPCam():
     
     def flashOn(self):
         torch_on = '/enabletorch'
-        IPCam.http.request.urlopen( self.url + torch_on)
+        self.httpRequest( self.url + torch_on)
     
     def flashOff(self):
         torch_off = '/disabletorch'
-        IPCam.http.request.urlopen( self.url + torch_off)
+        self.httpRequest( self.url + torch_off)
     
     def focus(self):
         focus = '/focus'
-        IPCam.http.request.urlopen( self.url + focus)
+        self.httpRequest( self.url + focus)
     
     def A_setFocus(self,focus_val,focus_min = 0,focus_max=13):
         focus_inc = 0.01
         focus_mode= '/settings/focusmode?set='+IPCam.manual
         focus_cam = '/settings/focus_distance?set='
-        IPCam.http.request.urlopen( self.url + focus_mode ) #setting focus mode to manula first
+        self.httpRequest( self.url + focus_mode ) #setting focus mode to manula first
         
         if focus_val >= focus_min and focus_val <= focus_max:
-            IPCam.http.request.urlopen( self.url + focus_cam + str(focus_val))
+            self.httpRequest( self.url + focus_cam + str(focus_val))
         elif focus_val < focus_min:
             return focus_val - focus_min
         else:
@@ -110,14 +110,14 @@ class IPCam():
         if focus_mode==IPCam.auto or focus_mode==IPCam.manual or \
         focus_mode==IPCam.macro or focus_mode==IPCam.smooth or focus_mode==IPCam.aggressive: 
            focus_mode= '/settings/focusmode?set='+focus_mode
-           IPCam.http.request.urlopen( self.url + focus_mode ) #setting focus mode
+           self.httpRequest( self.url + focus_mode ) #setting focus mode
            return 0
         else:
             return 1   #return 1 if invalid focus mode requested
            
     def noFocus(self):
         nofocus = '/nofocus'
-        IPCam.http.request.urlopen( self.url + nofocus)
+        self.httpRequest( self.url + nofocus)
         return 0
         
     
@@ -126,18 +126,18 @@ class IPCam():
         auto_cam='/settings/manual_sensor?set=off'
         manual_cam='/settings/manual_sensor?set=on'
         if sensor_mode==IPCam.auto:
-            IPCam.http.request.urlopen( self.url + auto_cam)
+            self.httpRequest( self.url + auto_cam)
         elif sensor_mode==IPCam.manual:
-            IPCam.http.request.urlopen( self.url + manual_cam)
+            self.httpRequest( self.url + manual_cam)
     
     def A_setIso(self,iso_val,iso_min = 100,iso_max=5699):
         iso_inc = 1
         iso_mode= '/settings/manual_sensor?set=on'
         iso_cam = '/settings/iso?set='
-        IPCam.http.request.urlopen( self.url + iso_mode ) #setting iso mode to manula first
+        self.httpRequest( self.url + iso_mode ) #setting iso mode to manula first
         
         if iso_val >= iso_min and iso_val <= iso_max:
-            IPCam.http.request.urlopen( self.url + iso_cam + str(iso_val))
+            self.httpRequest( self.url + iso_cam + str(iso_val))
         elif iso_val < iso_min:
             return iso_val - iso_min
         else:
@@ -148,10 +148,10 @@ class IPCam():
         shutter_inc = 1
         shutter_mode= '/settings/manual_sensor?set=on'
         shutter_cam = '/settings/frame_duration?set='
-        IPCam.http.request.urlopen( self.url + shutter_mode ) #setting shutter mode to manula first
+        self.httpRequest( self.url + shutter_mode ) #setting shutter mode to manula first
         
         if shutter_val >= shutter_min and shutter_val <= shutter_max:
-            IPCam.http.request.urlopen( self.url + shutter_cam + str(shutter_val))
+            self.httpRequest( self.url + shutter_cam + str(shutter_val))
         elif shutter_val < shutter_min:
             return shutter_val - shutter_min
         else:
@@ -160,23 +160,26 @@ class IPCam():
 
     def switchToFrontCam(self):
         fron_cam = '/settings/ffc?set=on'
-        IPCam.http.request.urlopen( self.url + fron_cam)
+        self.httpRequest( self.url + fron_cam)
         return 0
         
     def switchToRearCam(self):
         main_cam = '/settings/ffc?set=off'
-        IPCam.http.request.urlopen( self.url + main_cam)
+        self.httpRequest( self.url + main_cam)
         return 0
      
     def sensorData(self):
         sensor = '/sensors.json'
-        return(IPCam.http.request.urlopen( self.url + sensor))
+        return(self.httpRequest( self.url + sensor))
     
     def __del__(self):
         self.cap.release()
         IPCam.active_cameras -=1
         return 0
 
-  
+    def httpRequest(self,url):  
+        #print(url)
+        response= http.request('GET',url)
+        return response
 
    
